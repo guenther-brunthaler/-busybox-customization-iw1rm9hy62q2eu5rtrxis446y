fc3de0c71625e8714b9558e558dcb87f388737d3
Guenther Brunthaler <gb@xrasp.emgenxx69lwyn5ctlr4nl64ul.local>
2015-12-03 19:09:24 +0100

    Add applet symlinks to staging directory
    The script dynamically discovers which applets are already available as
    "real" commands, skipping symlink creation in this case.
    
    Otherwise a symlink for the applet is created as a command.
    
    This is helpful for uninstallation, because the symlinks can be removed
    together with the other installed files, rather than requiring to scan
    for and remove dead symlinks.

--- stage_installation.sh
+++ stage_installation.sh
@@ -45,3 +45,70 @@ mkdir "$sdir/usr/local/share"
 mkdir "$sdir/usr/local/share/doc"
 mkdir "$sdir/usr/local/share/doc/$stage"
 cp docs/BusyBox.txt "$sdir/usr/local/share/doc/$stage/$BB_LINK.txt"
+# Create symlinks for commands which are not otherwise available.
+./busybox --list | {
+	# Seed the list of search paths from $PATH.
+	ofs=$IFS; IFS=':'; set -- $PATH; IFS=$ofs
+	n=$#
+	while :
+	do
+		test -d "$1" && test -x "$1" && set -- "$@" "$1"
+		shift
+		n=`expr $n - 1` || break
+	done
+	# Add missing paths from a predefined hard-coded list.
+	for p0 in / /usr /usr/local "$HOME"
+	do
+		for p in bin sbin
+		do
+			p=${p0%%/}/$p
+			if test -d "$p" && test -x "$p"
+			then
+				# Make sure it is not a duplicate.
+				n=$#; found=false
+				while :
+				do
+					if test x"$1" = x"$p"
+					then
+						found=true; break
+					fi
+					set -- "$@" "$1"; shift
+					n=`expr $n - 1` || break
+				done
+				$found || set -- "$@" "$p"
+			fi
+		done
+	done
+	while read applet
+	do
+		# Search for a command with the same name in the search paths.
+		found=false
+		for p
+		do
+			a=$p/$applet
+			if test -f "$a" && test -x "$a"
+			then
+				# Found a matching command.
+				if test -L "$a"
+				then
+					# But it's a symlink. Resolve it.
+					a=`readlink -f -- "$a"`
+					a=`basename -- "$a"`
+					# Is symlink actually a reference to
+					# an older installed $BB_TARGET?
+					if test x"$a" != x"$BB_TARGET"
+					then
+						# No, then it's a real command.
+						# Skip symlink creation.
+						found=true; break
+					fi
+				else
+					found=true; break
+				fi
+			fi
+		done
+		$found && continue # Already present in search paths.
+		# Create an applet symlink for the missing command.
+		ln -s "$BB_TARGET" "$sdir/usr/local/bin/$applet"
+	done
+}
